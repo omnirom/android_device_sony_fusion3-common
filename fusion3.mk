b7a3407f635deea5ecf5eb88c8560bed07905616
iKillCypher <xzain69@gmail.com>
2013-11-10 20:10:07 +0800

    Remove Broken Radio Permission In 4.4 Yuga
    Change-Id: I2beb14a6ca9d4d9636170df6f20ece10df3e1c75

--- fusion3.mk
+++ fusion3.mk
@@ -125,8 +125,7 @@ PRODUCT_PACKAGES += \
 
 # FM Radio
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
-    frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml
+    $(COMMON_PATH)/rootdir/system/etc/init.qcom.fm.sh:system/etc/init.qcom.fm.sh
 
 PRODUCT_PACKAGES += \
     FmRadio
