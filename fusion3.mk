32a5562a3805b3e2dce0877ecd0f00b692623c73
Chirayu Desai <cdesai@cyanogenmod.org>
2014-11-02 10:59:37 +0530

    Inherit from msm8960-common
    * msm8960-common then inherits from qcom-common.
    * This doesn't change anything currently, just adds one more
      common tree in between - however, later on, we can start moving
      things to this new tree as needed, from both fusion3-common
      and qcom-common
    
    Change-Id: Ib0a5bca4569501e8504ae18b04c98f59130fe4be

--- fusion3.mk
+++ fusion3.mk
@@ -14,8 +14,8 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# qcom common
-$(call inherit-product, device/sony/qcom-common/qcom-common.mk)
+# inherit from msm8960-common
+$(call inherit-product, device/sony/msm8960-common/msm8960.mk)
 
 COMMON_PATH := device/sony/fusion3-common
 
