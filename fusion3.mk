b066c311a95909fb5fe5b4c0afb3e7cdcc43947b
Joe Wooster <jeebobs@btinternet.com>
2014-12-08 15:25:59 +0000

    fusion3: Remove audio resampler
    Change-Id: Idef204a698ec5fdb982d06a4b3aa8812bd28a903

--- fusion3.mk
+++ fusion3.mk
@@ -189,7 +189,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.audio.fluence.mode=endfire \
     persist.audio.handset.mic=analog \
     persist.audio.lowlatency.rec=false \
-    af.resampler.quality=255 \
     ro.qc.sdk.audio.fluencetype=none \
     lpa.decode=true
 
