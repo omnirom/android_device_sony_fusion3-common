02a5aca614faa9e854659f154313fd0effd47508
olivieer <yves.olli@gmail.com>
2015-07-12 20:35:38 +0200

    fusion3: Use the new qcom perfd
    Much better than using a hex'ed libqc-opt.
    
    Pulled from the Sony 5.0.2 (10.6.A.0.454) release.
    
    Change-Id: I7f8176cd92d97d7f53c5bbb2c4df556cc130afa0

--- fusion3.mk
+++ fusion3.mk
@@ -275,7 +275,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.extension_library=/vendor/lib/libqc-opt.so
+    ro.vendor.extension_library=/vendor/lib/libqti-perfd-client.so
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
